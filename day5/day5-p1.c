69e2e9b6404bea1ee11297f4aa929090daf8215c
Haruka-Hua <gtyl2006@163.com>
2024-12-26 20:07:35 +0800

    day5

---
+++ day5/day5-p1.c
@@ -0,0 +1,85 @@
+//
+// Created by tianyao on 24-12-23.
+//
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+struct RuleNode {
+    int former;
+    int later;
+    struct RuleNode *next;
+};
+typedef struct RuleNode node;
+node *rh=NULL, *rt=NULL;
+
+void AddRule(char *str) {
+    node *p=(node *)malloc(sizeof(node));
+    p->former=atoi(strtok(str,"|"));
+    p->later=atoi(strtok(NULL,"|"));
+    p->next=NULL;
+    if(rh==NULL) {
+        rh=p;
+        rt=p;
+    }
+    else {
+        rt->next=p;
+        rt=p;
+    }
+}
+bool cmp(const int x,const int y) {
+    node *current=rh;
+    while(current!=NULL) {
+        if(current->former==x && current->later==y) {
+            return 1;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+bool Right(int *update,int i) {
+    for(int j=0;j<i-1;j++) {
+        if(!cmp(update[j],update[j+1])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+int Update(char *str) {
+    int update[32];
+    int i=0;
+    char *token=strtok(str,",");
+    while(token) {
+        update[i]=atoi(token);
+        i++;
+        token=strtok(NULL,",");
+    }
+    if(Right(update,i)) return update[i/2];
+    else return 0;
+
+}
+void Del() {
+    while(rh!=NULL) {
+        node *temp=rh;
+        rh=rh->next;
+        free(temp);
+    }
+}
+int main() {
+    FILE *fp=fopen("D:\\CLionProjects\\advent-of-code\\"
+                   "day5\\day5-input","r");
+    char str[200];
+    int sum=0;
+    fscanf(fp,"%s",str);
+    while(strchr(str,'|')!=NULL) {
+        AddRule(str);
+        fscanf(fp,"%s",str);
+    }
+    do {
+        sum+=Update(str);
+    }while(fscanf(fp,"%s",str)!=EOF);
+    printf("%d",sum);
+    fclose(fp);
+    Del();
+    return 0;
+}
